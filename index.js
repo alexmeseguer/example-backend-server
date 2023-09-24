12c6a315da9ff68ff8961f92721488208e3f1e39
alejandro-meseguer <alex.meseguersilvestre@gmail.com>
2023-09-24 13:28:34 +0200

    server js

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+// const jsonServe = require("json-serve");
+// const cors = require("cors");
